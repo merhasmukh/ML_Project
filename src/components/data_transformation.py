b02d16ecce7c37fff1815a7dc609de5f91d3b6e6
mhgn <mhgn0001@gmail.com>
2023-03-14 10:58:45 +0530

    data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -86,7 +86,7 @@ class DataTransformation:
             )
 
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.fit_transform(input_feature_test_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
             train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)
             ]
