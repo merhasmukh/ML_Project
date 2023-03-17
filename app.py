e0749337a301495272799e2edb3ee741fc24df6e
mhgn <mhgn0001@gmail.com>
2023-03-17 10:01:44 +0530

    predict pipeline added

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
-from src.components.model_trainer import ModelTrainerConfig,Modeltrainer
-from src.components.data_ingestion import DataIngestion
 
-obj=DataIngestion()
-train_data,test_data=obj.initiate_data_ingestion()
+from flask import Flask,request ,render_template
 
-data_transformation=DataTransformation()
-train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+import numpy as np
+import pandas as pd
+import requests
+from sklearn.preprocessing import StandardScaler
 
-model_trainer=Modeltrainer()
-print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+app=Flask(__name__)
+
+
+@app.route("/",methods=["POST","GET"])
+def inedx():
+    return render_template("index.html")
+
+@app.route("/predictdata",methods=["POST","GET"])
+def predict_datapoint():
+    if request.method=="GET":
+        return render_template("home.html")
+    
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('race_ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score')),
+
+
+
+
+        )
+
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+
+        predict_pipeline=PredictPipeline()
+        results=predict_pipeline.predict(pred_df)
+        return render_template("home.html",results=results[0])
+        # return render_template("index.html")
+
+
+if __name__=="__main__":
+    
+    app.run('0.0.0.0',debug=True)
